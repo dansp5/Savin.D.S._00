e7c568bc6da5cd9408c56e50c366137e26daf2f4
Dans <danson2off@yandex.ru>
2022-12-29 22:50:55 +0300

    On branch master  Initial commit 	new file:   labb1.sln 	new file:   labb1/Cell.cs 	new file:   labb1/Program.cs 	new file:   labb1/bin/Debug/net5.0/labb1.deps.json 	new file:   labb1/bin/Debug/net5.0/labb1.dll 	new file:   labb1/bin/Debug/net5.0/labb1.exe 	new file:   labb1/bin/Debug/net5.0/labb1.pdb 	new file:   labb1/bin/Debug/net5.0/labb1.runtimeconfig.dev.json 	new file:   labb1/bin/Debug/net5.0/labb1.runtimeconfig.json 	new file:   labb1/bin/Debug/net5.0/ref/labb1.dll 	new file:   labb1/labb1.csproj 	new file:   labb1/labb1.csproj.user 	new file:   labb1/obj/Debug/net5.0/.NETCoreApp,Version=v5.0.AssemblyAttributes.cs 	new file:   labb1/obj/Debug/net5.0/apphost.exe 	new file:   labb1/obj/Debug/net5.0/labb1.AssemblyInfo.cs 	new file:   labb1/obj/Debug/net5.0/labb1.AssemblyInfoInputs.cache 	new file:   labb1/obj/Debug/net5.0/labb1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   labb1/obj/Debug/net5.0/labb1.assets.cache 	new file:   labb1/obj/Debug/net5.0/labb1.csproj.AssemblyReference.cache 	new file:   labb1/obj/Debug/net5.0/labb1.csproj.CoreCompileInputs.cache 	new file:   labb1/obj/Debug/net5.0/labb1.csproj.FileListAbsolute.txt 	new file:   labb1/obj/Debug/net5.0/labb1.dll 	new file:   labb1/obj/Debug/net5.0/labb1.genruntimeconfig.cache 	new file:   labb1/obj/Debug/net5.0/labb1.pdb 	new file:   labb1/obj/Debug/net5.0/ref/labb1.dll 	new file:   labb1/obj/labb1.csproj.nuget.dgspec.json 	new file:   labb1/obj/labb1.csproj.nuget.g.props 	new file:   labb1/obj/labb1.csproj.nuget.g.targets 	new file:   labb1/obj/project.assets.json 	new file:   labb1/obj/project.nuget.cache 	new file:   labb1/stack.cs
     Untracked files:
    	.vs/

---
+++ labb1/Cell.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace labb1
+{
+    // Ячейка стека
+    class Cell
+    {
+        //Значение ячейки(у первого элемента 0)
+        public int Value = 0;
+        //ссылка на предыдущую ячеку(у первого элемента null)
+        public Cell Prev = null;
+        // при создании ячейки отправляются значение и ссылка 
+        public Cell(int value=0, Cell prev=null)
+        {
+            Value = value;
+            Prev = prev;
+        }
+    }
+}
---
+++ labb1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace labb1
+{
+    class Program
+    {   
+        // пирамидальная сортировка
+        static void Sort(stack st)
+        {
+            // создаем стек
+            st.N++;
+            // Получаем его размер
+            int size = st.Size();
+            st.N+=2;
+            // Пирамидальная сортировка 
+            for (int i = 0; i < size; i++)
+            {
+                st.N+=3;
+                // откладываем элемент
+                for (int j = size-i; j > 1; j--)
+                {
+                    st.N+=5;
+                    // если сын больше родителя, то меняем местами
+                    if (st.GetS(j)<st.GetS(j/2))
+                    {
+                        st.N+=3;
+                        st.Swap(j, j / 2);
+                    }
+                    st.N++;
+
+                }
+                st.N+=3;
+                st.Push(st.Pop());
+            }
+            Console.WriteLine("Количество операций: "+st.N);
+        }
+        static void Main(string[] args)
+        {
+            Int64 t =0 ;
+            Random r = new Random();
+            for (int j = 0; j <10 ; j++)
+            {
+                Console.WriteLine("Тест №"+(j+1)+"\n Количество элементов: "+(1000+j*200));
+                stack a = new stack();
+                for (int i = 0; i < 1000+j*200; i++)
+                {
+                    a.Push(r.Next(1,100));
+                }
+                a.N = 0;
+                t=Environment.TickCount;
+                Sort(a);
+                t = Environment.TickCount - t;
+                Console.WriteLine("Время выполнения: "+t);
+            }
+        }
+    }
+}
